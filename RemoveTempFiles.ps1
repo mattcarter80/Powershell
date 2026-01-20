06d4fb09b3454d4d88514584e260fc981d97d55b
mattcarter80 <matt.carter80@outlook.com>
2026-01-20 07:46:46 +0000

    Modify temp file removal script for user profiles
    Updated user profile paths to be dynamic.

--- RemoveTempFiles.ps1
+++ RemoveTempFiles.ps1
@@ -1,7 +1,8 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -ErrorAction SilentlyContinue
 
-$TempFiles = @("C:\Windows\Prefetch\*","C:\Windows\Temp\*","C:\Users\mattc\AppData\Local\Google\Chrome\User Data\Default\Cache\Cache_Data\*","C:\Users\mattc\AppData\Local\Microsoft\Edge\User Data\Default\Cache\Cache_Data","C:\Users\mattc\AppData\Local\Temp")
+$TempFiles = @("C:\Windows\Prefetch\*","C:\Windows\Temp\*","C:\Users\<user-profile>\AppData\Local\Google\Chrome\User Data\Default\Cache\Cache_Data\*","C:\Users\<user-profile>\AppData\Local\Microsoft\Edge\User Data\Default\Cache\Cache_Data","C:\Users\<user-profile>\AppData\Local\Temp")
 
 Remove-Item -Path $TempFiles -ErrorAction SilentlyContinue -Recurse -Force
+
 
 Pause
